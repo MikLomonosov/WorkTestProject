05e1cfa78ffa94ded45115fb97b248412c28ee87
Victor <53020213+Victor@users.noreply.github.com>
2023-04-22 12:10:36 +0300

    Add project files.

---
+++ WorkTestProject.Desktop/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WorkTestProject.Desktop.ViewModels;
+
+namespace WorkTestProject.Desktop
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private MainWindowViewModels startViewModel;
+        protected async override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            startViewModel = new MainWindowViewModels();
+            new MainWindow
+            {
+                DataContext = startViewModel
+            }.Show();
+        }
+    }
+}
---
+++ WorkTestProject.Desktop/ViewModels/BaseVM/BaseViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkTestProject.Desktop.ViewModels.BaseVM
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        #region INPC
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string property = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+        #endregion
+
+        protected BaseViewModel ChangeContext(BaseViewModel newContext)
+        {
+            return newContext;
+        }
+    }
+}
